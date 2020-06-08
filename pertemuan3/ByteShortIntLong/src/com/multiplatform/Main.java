6434e5de042ab73d2018afcdee7280167b4117e6
jeinecl <jeineclaudia@gmail.com>
2020-06-08 17:03:52 +0800

    tugas 2 byteshortlong

--- pertemuan3/ByteShortIntLong/src/com/multiplatform/Main.java
+++ pertemuan3/ByteShortIntLong/src/com/multiplatform/Main.java
@@ -7,11 +7,31 @@ public class Main {
 
 	    int myMaxIntValue = Integer.MAX_VALUE;
 	    int myMinIntValue = Integer.MIN_VALUE;
-        System.out.println("Integer Maximum Value = " + myMaxIntValue);
-        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum INT Value = " + myMaxIntValue);
+        System.out.println("Integer Minimum INT Value = " + myMinIntValue);
+        System.out.println("Overflow max INT Value = " + (myMaxIntValue + 1));
+        System.out.println("Underflow min INT Value = " + (myMinIntValue - 1));
 
-        System.out.println("Overflow max Value = " + (myMaxIntValue + 1));
-        System.out.println("Underflow min Value = " + (myMinIntValue - 1));
+        int myMaxByteValue = Byte.MAX_VALUE;
+        int myMinByteValue = Byte.MIN_VALUE;
+        System.out.println("Byte Maximum BYTE Value = " + myMaxByteValue);
+        System.out.println("Byte Minimum BYTE Value = " + myMinByteValue);
+        System.out.println("Overflow max BYTE Value = " + (myMaxByteValue + 1));
+        System.out.println("Underflow min BYTE Value = " + (myMinByteValue - 1));
+
+        int myMaxShortValue = Short.MAX_VALUE;
+        int myMinShortValue = Short.MIN_VALUE;
+        System.out.println("Short Maximum SHORT Value = " + myMaxShortValue);
+        System.out.println("Short Minimum SHORT Value = " + myMinShortValue);
+        System.out.println("Overflow max SHORT Value = " + (myMaxShortValue + 1));
+        System.out.println("Underflow min SHORT Value = " + (myMinShortValue - 1));
+
+        long myMaxLongValue = Long.MAX_VALUE;
+        long myMinLongValue = Long.MIN_VALUE;
+        System.out.println("Integer Maximum LONG Value = " + myMaxLongValue);
+        System.out.println("Integer Minimum LONG Value = " + myMinLongValue);
+        System.out.println("Overflow max LONG Value = " + (myMaxLongValue + 1));
+        System.out.println("Underflow min LONG Value = " + (myMinLongValue - 1));
 
     }
 }
