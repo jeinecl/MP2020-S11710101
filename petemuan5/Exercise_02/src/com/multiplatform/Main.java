93026f25672042628bc10529c21cce475995c6db
jeinecl <jeineclaudia@gmail.com>
2020-06-10 16:16:06 +0800

    exercise_02

---
+++ petemuan5/Exercise_02/src/com/multiplatform/Main.java
@@ -0,0 +1,14 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //Exercise_02; Lambani, Jeine
+        //1:
+        double myPoundsNumber = 88d;
+        //2:
+        double kilogramsResult = myPoundsNumber * 0.45359237;
+        //3:
+        System.out.println(myPoundsNumber + " pounds in kilograms = " + kilogramsResult);
+    }
+}
