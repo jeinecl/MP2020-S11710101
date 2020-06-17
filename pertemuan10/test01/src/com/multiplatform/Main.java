84cd6f0dd1ed76bc0952b158f188131f95080401
jeinecl <jeineclaudia@gmail.com>
2020-06-17 16:18:43 +0800

    pertemuan10 test01 number2

--- pertemuan10/test01/src/com/multiplatform/Main.java
+++ pertemuan10/test01/src/com/multiplatform/Main.java
@@ -4,10 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// Lambani, Jeine Claudia; Test 01
+        //1.
+        System.out.println("1. Barking Dog:");
         System.out.println(shouldWakeUp(true, 1));
         System.out.println(shouldWakeUp(false, 2));
         System.out.println(shouldWakeUp(true, 8));
         System.out.println(shouldWakeUp(true, -1));
+        //2.
+        System.out.println("2. Cat Playing:");
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
     }
     //1. Barking dog
     public static boolean shouldWakeUp(boolean barking, int hourOfDay){
@@ -21,6 +28,22 @@ public class Main {
         }
         else{
             return  false;
+        }
+    }
+    //2. Playing cat
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        else
+        if (temperature >= 25 && temperature <= 35) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
