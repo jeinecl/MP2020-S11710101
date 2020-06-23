3f68976ec32232d59decdfcc24a3b2093635b3cf
jeinecl <jeineclaudia@gmail.com>
2020-06-23 17:27:10 +0800

    pertemuan13

--- pertemuan13/untitled/src/com/multiplatform/Account.java
+++ pertemuan13/untitled/src/com/multiplatform/Account.java
@@ -7,16 +7,25 @@ public class Account {
     private String emailAddress;
     private String phoneNumber;
 
+    //Constructor
     public Account(){
         //System.out.println("Constructor without parameter is running");
-        this("99999999", )
+        this("99999999", 0.0, "Default name", "Default email", "Default phone number" );
     }
+
     public Account(String number, double balance, String customerName, String emailAddress, String phoneNumber){
         this.number = number;
         this.balance = balance;
         this.costumerName = customerName;
         this.emailAddress = emailAddress;
         this.phoneNumber = phoneNumber;
+    }
+
+    public Account(String costumerName, String emailAddress, String phoneNumber) {
+//        this.costumerName = costumerName;
+//        this.emailAddress = emailAddress;
+//        this.phoneNumber = phoneNumber;
+        this("999999", 0.0, costumerName, emailAddress, phoneNumber);
     }
 
     public String getNumber() {
@@ -33,43 +42,43 @@ public class Account {
             System.out.println("Insufficient funds. Withdrawal not processes");
         }else{
             this.balance -= amount;
-            System.out.println("Withdrawal of " + amount + " processed. Remaining balance is " + this.balance;
+            System.out.println("Withdrawal of " + amount + " processed. Remaining balance is " + this.balance);
         }
     }
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
+//    public void setNumber(String number) {
+//        this.number = number;
+//    }
 
     public double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+//    public void setBalance(double balance) {
+//        this.balance = balance;
+//    }
 
     public String getCostumerName() {
         return costumerName;
     }
 
-    public void setCostumerName(String costumerName) {
-        this.costumerName = costumerName;
-    }
+//    public void setCostumerName(String costumerName) {
+//        this.costumerName = costumerName;
+//    }
 
     public String getEmailAddress() {
         return emailAddress;
     }
 
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
+//    public void setEmailAddress(String emailAddress) {
+//        this.emailAddress = emailAddress;
+//    }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
 }
--- pertemuan13/untitled/src/com/multiplatform/Main.java
+++ pertemuan13/untitled/src/com/multiplatform/Main.java
@@ -4,17 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Account johnsAcount = new Account("111111," 200.00, "John", "john@mail.com", );
-        johnsAcount.setCostumerName("Bob");
-        johnsAcount.setNumber("123456789");
-        johnsAcount.setBalance(100.00);
-        johnsAcount.setEmailAddress("bob@mail.com");
-        johnsAcount.setPhoneNumber("(62)811324234");
+        Account johnsAcount = new Account("1111111", 200.00, "John", "john@mail,com", "12345678");
+//        johnsAcount.setCostumerName("Bob");
+//        johnsAcount.setNumber("123456789");
+//        johnsAcount.setBalance(100.00);
+//        johnsAcount.setEmailAddress("bob@mail.com");
+//        johnsAcount.setPhoneNumber("(62)811324234");
 
         System.out.println("Customer name: " + johnsAcount.getCostumerName());
         System.out.println("Balance: " + johnsAcount.getBalance());
         johnsAcount.deposit(50.0);
         System.out.println("Balance: " + johnsAcount.getBalance());
         johnsAcount.withdrawal(155.0);
+
+        //Object for default constructor
+        Account bobsAccount = new Account();
+        System.out.println("Customer name: " + bobsAccount.getCostumerName());
+        System.out.println("Balance: " + bobsAccount.getBalance());
+
+        Account mikesAccount = new Account("Mike", "mike@mail.com", "123123123");
+        System.out.println("Customer name: " + mikesAccount.getCostumerName());
+        System.out.println("Balance: " + mikesAccount.getBalance());
+
     }
 }
